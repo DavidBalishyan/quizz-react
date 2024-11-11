e0efab0119ecd54a0a8ad05d499357c826483ae2
David Balishyan <128315366+DavidBalishyan@users.noreply.github.com>
2024-11-11 23:04:43 +0400

    Add files via upload

---
+++ src/questions.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+      questionText: "What is the capital of France?",
+      answerOptions: [
+        { answerText: "Berlin", isCorrect: false },
+        { answerText: "Madrid", isCorrect: false },
+        { answerText: "Paris", isCorrect: true },
+        { answerText: "Lisbon", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Who is CEO of Tesla?",
+      answerOptions: [
+        { answerText: "Jeff Bezos", isCorrect: false },
+        { answerText: "Elon Musk", isCorrect: true },
+        { answerText: "Bill Gates", isCorrect: false },
+        { answerText: "Tony Stark", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is the capital of Armenia?",
+      answerOptions: [
+        { answerText: "Pyongyang", isCorrect: false },
+        { answerText: "Munchen", isCorrect: false },
+        { answerText: "Erevean", isCorrect: true },
+        { answerText: "Washington", isCorrect: false },
+      ],
+    }
+  ];
+  
+  export default questions;
+  
